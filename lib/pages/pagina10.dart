59a29f3c2a78d4003f6f81a0556a07adab8c82f6
Javierub32 <javierub38@gmail.com>
2024-08-05 19:59:27 +0200

    Refactorizamos el código para guardar bien la info del pokemon

--- lib/pages/pagina10.dart
+++ lib/pages/pagina10.dart
@@ -15,8 +15,8 @@ class _Pagina10_UsoDeAPIState extends State<Pagina10_UsoDeAPI> {
   late Future<List<Pokemon>> _listadoPokemons; //* Hacemos una lista de pokemon que inicializaremos más adelante.
   
   //La función es asíncrona porque estamos haciendo una petición a un servidor y puede tardar en responder.
-  Future<List<Pokemon>> getPokemon() async{  
-    final response = await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=5"));
+  Future<List<Pokemon>> fetchPokemon() async{  
+    final response = await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=10"));
     List<Pokemon> pokemons = [];
     
     if (response.statusCode == 200) { //El codigo 200 significa que la petición fue exitosa.
@@ -27,23 +27,13 @@ class _Pagina10_UsoDeAPIState extends State<Pagina10_UsoDeAPI> {
       for (var pokemon in JSonData['results']) {
         String nombrePokemon = pokemon['name'];
         String referenciaPokemon = pokemon['url'];
+        String pokemonImageUrl = await fetchPokemonImageUrl(referenciaPokemon);
 
-        //Buscamos la imagen del pokemon
-        final responseImagen = await http.get(Uri.parse(referenciaPokemon));
+        Pokemon pokemontemporal = Pokemon(name: nombrePokemon, urlImagen: pokemonImageUrl);
+        pokemons.add(pokemontemporal);
 
-        if (responseImagen.statusCode == 200) {
-          String bodyImagen = utf8.decode(responseImagen.bodyBytes);
-          final JSonDataImagen = json.decode(bodyImagen);
-          String urlImagenPokemon = JSonDataImagen['sprites']['front_default'];
-
-          Pokemon pokemon = Pokemon(name: nombrePokemon, urlImagen: urlImagenPokemon);
-          pokemons.add(pokemon);
-          
-          print('Pokemon: $nombrePokemon Imagen: $urlImagenPokemon');
-
-        } else {
-          print('Request failed with status: ${responseImagen.statusCode}');
-        }
+        //TODO: Eliminar
+        print('Nombre: $nombrePokemon - Imagen: $pokemonImageUrl');
       }
 
     } else {
@@ -56,7 +46,7 @@ class _Pagina10_UsoDeAPIState extends State<Pagina10_UsoDeAPI> {
   @override
   void initState(){
     super.initState();
-    _listadoPokemons = getPokemon();
+    _listadoPokemons = fetchPokemon();
     _listadoPokemons.ignore(); //! Eliminar
   }
    
@@ -75,7 +65,7 @@ class Pokemon{
   Pokemon({required this.name, required this.urlImagen});
 }
 
-Future<String> buscarurlImagenPokemon(String referenciaPokemon) async {
+Future<String> fetchPokemonImageUrl(String referenciaPokemon) async {
   final responseImagen = await http.get(Uri.parse(referenciaPokemon));
   String urlImagenPokemon = '';
   
